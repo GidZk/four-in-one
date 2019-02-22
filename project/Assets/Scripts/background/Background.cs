9dfd0230ea098977b1b3fb9c2d38e53cf17cc90c
John Lindström Gidskehaug <johnlindstrom92@hotmail.com>
2019-02-22 16:12:50 +0100

    added scrollign pictureeeee

---
+++ project/Assets/Scripts/background/Background.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    Renderer rend;
+    public float backgroundSpeed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * backgroundSpeed, 0);
+
+        rend.material.mainTextureOffset = offset;
+
+    }
+}
