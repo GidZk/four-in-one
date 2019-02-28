7e550520c5fb1b7ab7f3273f3b813ad3c26846c5
Alice <alice@granssnittsarna.se>
2019-02-28 13:29:04 +0100

    now fish acting dead, but i will fix it next commit

---
+++ project/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    //public Rigidbody rb;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+    }
+
+    void FixedUpdate() //For physics
+    {
+  controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+  //      rb.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+
+}
