006716aa7ef875066a76b6ed5b93535e778789c5
Erik Lundin <erilundi@student.chalmers.se>
2019-03-01 13:26:38 +0100

    added basic touch controll

--- project/Assets/Scripts/PlayerMovement.cs
+++ project/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     float verticalMove = 0f;
+    public Vector2 touchOrigin = -Vector2.one;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +20,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      #if UNITY_STANDALONE || UNITY_WEBPLAYER // to enable both type of controllers
+
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
+      #else
+      if(Input.touchCount > 0){
+        Touch myTouch = Input.touches[0];
+
+        if(myTouch.phase == TouchPhase.Began){
+          touchOrigin = myTouch.position;
+        }
+        else if(myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0){
+          Vector2 touchEnd = myTouch.position;
+          float x = touchEnd.x - touchOrigin.x;
+          touchOrigin.x = -1;
+          
+            horizontalMove = x > 0 ? 1 : -1;
+      
+        }
+        #endif
+      }        
 
     }
 
