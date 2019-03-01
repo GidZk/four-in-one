19c22e333f1e0e7940924aa1ea3866206ae88b35
John Lindström Gidskehaug <johnlindstrom92@hotmail.com>
2019-03-01 14:58:52 +0100

    rearranged scene, added camera for networking.

---
+++ project/Assets/Networking Tutorial/PlayerObject.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour
+// here goes the data for each attached object
+{
+
+    public GameObject PlayerUnitPrefab;
+    // Start is called before the first frame update
+    void Start() {
+        Instantiate(PlayerUnitPrefab);
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
