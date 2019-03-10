ebe5af0293d9e9f8f61364fcd87359fc472d7946
Erik Lundin <erilundi@student.chalmers.se>
2019-03-10 20:00:08 +0100

    implemented simple touch buttons

--- project/Assets/Scripts/PlayerMovement.cs
+++ project/Assets/Scripts/PlayerMovement.cs
@@ -20,27 +20,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      #if UNITY_STANDALONE || UNITY_WEBPLAYER // to enable both type of controllers
-
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
-      #else
-      if(Input.touchCount > 0){
-        Touch myTouch = Input.touches[0];
-
-        if(myTouch.phase == TouchPhase.Began){
-          touchOrigin = myTouch.position;
-        }
-        else if(myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0){
-          Vector2 touchEnd = myTouch.position;
-          float x = touchEnd.x - touchOrigin.x;
-          touchOrigin.x = -1;
-          
-            horizontalMove = x > 0 ? 1 : -1;
       
-        }
-        #endif
-      }        
 
     }
 
---
+++ project/Assets/Scripts/playerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+    private float hInput = 0;
+    private float vInput = 0;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    //void Update(){
+        //moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //Move(hInput);
+      //   }
+
+    void FixedUpdate (){
+        //uncomment for arrows instead
+        //Move(Input.GetAxisRaw("Horizontal"));
+            
+        Move(hInput, vInput);
+         
+       
+    }
+    
+    public void Move ( float horizontalInput, float verticalInput){
+
+        Vector2 moveVelocity = rb.velocity;
+        moveVelocity.x = horizontalInput * moveSpeed;
+        moveVelocity.y  = verticalInput * moveSpeed;
+        rb.velocity = moveVelocity;
+
+    } 
+
+    public void HorizontalMovement(float horizontalInput){
+        hInput = horizontalInput;
+    }
+    public void VerticalMovement(float VerticalInput){
+        vInput = VerticalInput;
+    }
+}
