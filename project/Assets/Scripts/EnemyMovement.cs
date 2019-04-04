b675b71934f692bd58c65cb495fef88d7d78736e
Alice <alice@granssnittsarna.se>
2019-04-04 13:43:42 +0200

    solved runtime errors

--- project/Assets/Scripts/EnemyMovement.cs
+++ project/Assets/Scripts/EnemyMovement.cs
@@ -29,23 +29,23 @@ public class EnemyMovement : MonoBehaviour
         transform.Translate(Vector2.left * Time.deltaTime * speed);
 
 
-        if (up == true)
+        if (up == true && gameObject == shark )
         {
             shark.transform.Translate(Vector2.up * Time.deltaTime * speed);
         }
 
-        if (up == false)
+        if (up == false && gameObject == shark )
         {
             shark.transform.Translate(Vector2.down * Time.deltaTime * speed);
         }
 
         // if (BoundaryController.  object.objectcollide Equals(true))
-        if (transform.position.y > 4)
+        if (transform.position.y > 4 && gameObject == shark)
         {
             up = false;
         }
 
-        if (transform.position.y < -13)
+        if (transform.position.y < -13 && gameObject == shark)
         {
             up = true;
         }
