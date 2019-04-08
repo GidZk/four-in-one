41241a8b72a23111654a082d938dcf22748b2b4f
Magnus Wamby <wamby@student.chalmers.se>
2019-04-08 10:42:36 +0200

    Implement basic listener in networking

--- project/Assets/Scripts/Teams.cs
+++ project/Assets/Scripts/Teams.cs
@@ -24,7 +24,7 @@ class TeamUtil
 
     public static Team FromString(String s)
     {
-        if (Enum.TryParse(s, true, out Team team))
+        if (Enum.TryParse(s, true, out Teamx team))
         {
             return team;
         }
