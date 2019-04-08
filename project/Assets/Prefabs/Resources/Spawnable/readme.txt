b654efd691b00c3a9b0ed6ea8a795839139dce8b
Magnus Wamby <wamby@student.chalmers.se>
2019-04-08 17:20:42 +0200

    implement demo network connected objects

--- project/Assets/Prefabs/Resources/Spawnable/readme.txt
+++ project/Assets/Prefabs/Resources/Spawnable/readme.txt
@@ -1,3 +1,5 @@
+DOES NOT CURRENTLY WORK, MAY BE DIFFICULT TO IMPLEMENT IN A SYSTEM INDEPENDENT WAY  
+
 All prefabs in this folder will automatically be registered to the ClientScene
 so that they can be Spawn()'d (i.e. they can be instantiated across all clients
 from the server). 
---
+++ project/Assets/RandomMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class RandomMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Random.value < 0.10)
+        {
+            rb.AddForce(Random.insideUnitCircle * (float) (0.1 + Random.value) * 20);
+        }
+    }
+}
