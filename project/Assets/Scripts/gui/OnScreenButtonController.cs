f6f1ed526c70a4597c2b42bcca636c19f9c8b572
Magnus Wamby <wamby@student.chalmers.se>
2019-04-16 11:12:36 +0200

    Improves on screen buttons

---
+++ project/Assets/Scripts/gui/OnScreenButtonController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class OnScreenButtonController : MonoBehaviour
+{
+    private bool left;
+    private bool right;
+    private bool up;
+    private bool down;
+
+    public Canvas upDownCanvas;
+    public Canvas leftRightCanvas;
+
+    private List<InputListener> _inputListeners;
+
+
+    private void Awake()
+    {
+        _inputListeners = new List<InputListener>();
+
+        var nc = NetworkController.Instance;
+        if (nc != null)
+        {
+            _inputListeners.Add(nc);
+            // TODO improve this such that "roles" aren't solely determined by net id
+            upDownCanvas.enabled = nc.NetworkId == 0;
+            leftRightCanvas.enabled = nc.NetworkId == 3;
+        }
+    }
+
+    public void OnLeftPressed()
+    {
+        left = true;
+        _inputListeners.ForEach(it => it.OnHorizontalMovementInput(-1));
+    }
+
+    public void OnRightPressed()
+    {
+        right = true;
+        _inputListeners.ForEach(it => it.OnHorizontalMovementInput(1));
+    }
+
+    public void OnDownPressed()
+    {
+        down = true;
+        _inputListeners.ForEach(it => it.OnVerticalMovementInput(-1));
+    }
+
+    public void OnUpPressed()
+    {
+        up = true;
+        _inputListeners.ForEach(it => it.OnVerticalMovementInput(1));
+    }
+
+    public void OnLeftReleased()
+    {
+        left = false;
+        if (!right) _inputListeners.ForEach(it => it.OnHorizontalMovementInput(0));
+    }
+
+    public void OnRightReleased()
+    {
+        right = false;
+        if (!left) _inputListeners.ForEach(it => it.OnHorizontalMovementInput(0));
+    }
+
+    public void OnDownReleased()
+    {
+        down = false;
+        if (!up) _inputListeners.ForEach(it => it.OnVerticalMovementInput(0));
+    }
+
+    public void OnUpReleased()
+    {
+        up = false;
+        if (!down) _inputListeners.ForEach(it => it.OnVerticalMovementInput(0));
+    }
+}
--- project/Assets/Scripts/player/playerController.cs
+++ project/Assets/Scripts/player/playerController.cs
@@ -4,21 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class playerController : MonoBehaviour, InputListener {
-
-    
-    public float moveForce;
+public class playerController : MonoBehaviour, InputListener
+{
     private Rigidbody2D rb;
-    private Vector2 moveInput;
-    //private Vector2 moveVelocity;
-    private float hInput = 0;
-    private float vInput = 0;
-    
-  
 
-    private NetworkController nwController;
-    
-    public static playerController instance { get; private set; }
+
+    [SerializeField] private float _hInput;
+    [SerializeField] private float _vInput;
+
+    public const float SpeedFactorConstant = 45f;
+
+    public static playerController Instance { get; private set; }
 
 
     // Start is called before the first frame update
@@ -26,85 +22,52 @@ public class playerController : MonoBehaviour, InputListener {
 
     private void Awake()
     {
-        if (instance != null)
+        if (Instance != null)
         {
             throw new Exception("Instance is not null ");
         }
 
-        instance = this;
+        Instance = this;
     }
 
-    void Start(){
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
-        nwController = NetworkAssets.GetController();
-        nwController.Register(this);
+        NetworkController.Instance.Register(this);
     }
-    
-   
-    // ============ old code to be removed / updated for touches ================== 
-    /*
-    void Update(){
-        if(Input.touchCount > 0){
-            Touch touch = Input.GetTouch(0);
-            if(touch.phase == TouchPhase.Began){
-                Move(hInput, vInput);
-            }
-            else if(touch.phase == TouchPhase.Ended){
-                Debug.Log(hInput);
-                Move(0,0);
-            }
-                
-        }
-        //moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        //Move(hInput);
-    }
-    // ==============================================================================
-    */
-    // ============ old code to be removed / updated for touches ================== // 
+
+
     void FixedUpdate()
     {
-
-        //if (RectTransformUtility.RectangleContainsScreenPoint(
-                //           rightRect, data.position, data.pressEventCamera
-        //   )
-
+        if (Math.Abs(_hInput) < 0.1f)
         {
-            //targetRectArea is the rectangle transform you want your
-            //input to be inside, the above will return true if it is inside.
+        } // decelerate maybe
+        else
+        {
+            rb.AddForce(new Vector2(_hInput, 0) * SpeedFactorConstant);
         }
 
-
-    
-
-        // delegate movement to server in msg passing system
-        if (Input.GetKey(KeyCode.UpArrow))
-            nwController.OnVerticalMovementInput(moveForce);
-        if (Input.GetKey(KeyCode.DownArrow))
-            nwController.OnVerticalMovementInput(-moveForce);
-       // if (Input.GetKey(KeyCode.RightArrow))
-         //   nwController.OnHorizontalMovementInput(moveForce);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            nwController.OnHorizontalMovementInput(-moveForce);
-
-        
+        if (Math.Abs(_vInput) < 0.1f)
+        {
+        } // decelerate maybe
+        else
+        {
+            rb.AddForce(new Vector2(0, _vInput) * SpeedFactorConstant);
+        }
     }
-
-
- 
-
 
 
     // this method will be called by the client that has the server locally,
     // after a remote client has commanded the client which has the server to do so.
     public void OnHorizontalMovementInput(float value)
-  {
-      rb.AddForce(new Vector2(value,0));
-  }
-  
-  public void OnVerticalMovementInput(float value)
-  {
-      rb.AddForce(new Vector2(0,value));
-  }
+    {
+        _hInput = value;
+    }
+
+    public void OnVerticalMovementInput(float value)
+    {
+        _vInput = value;
+    }
 
     public void LinearDrag(float value)
     {
@@ -112,42 +75,32 @@ public class playerController : MonoBehaviour, InputListener {
     }
 
 
-
     public void OnCannonAngleInput(float value)
-  {
-      throw new System.NotImplementedException();
-  }
+    {
+    }
 
-  public void OnCannonLaunchInput(float value)
-  {
-      throw new System.NotImplementedException();
-  }
-  
-  
-  // Invoked on collision
-  void OnCollisionEnter2D(Collision2D coll)
-  {
-      if (coll.gameObject.tag == "crabplast")
-      {
-          Debug.Log($"{this} --a collision between player and crab. ");
+    public void OnCannonLaunchInput(float value)
+    {
+    }
 
-          Destroy(coll.gameObject);
 
-          //Add 1 point each time the starfish(object that gives points) collides
+    // Invoked on collision
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("crabplast"))
+        {
+            Debug.Log($"{this} --a collision between player and crab. ");
 
-          // with the object this script is attached to
-          AddScore.scoreValue++;
+            Destroy(coll.gameObject);
 
-          //add time to the timebar when catching crab
-          Timer.timeLeft++;
-          Debug.Log("score = " + AddScore.scoreValue);
-      }
-  }
+            //Add 1 point each time the starfish(object that gives points) collides
 
-  
-  
-  
-  
-  
+            // with the object this script is attached to
+            AddScore.scoreValue++;
+
+            //add time to the timebar when catching crab
+            Timer.timeLeft++;
+            Debug.Log("score = " + AddScore.scoreValue);
+        }
+    }
 }
-
