c48b9840bc3d51ea5408ddab1b174f397bb5d8c8
Magnus Wamby <wamby@student.chalmers.se>
2019-04-25 18:05:02 +0200

    implement a timer and game end

---
+++ project/Assets/GameTimer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  Behaviour used to move the submarine displaying progressed time
+/// </summary>
+public class GameTimer : MonoBehaviour
+{
+    public float MaxTime = 20f;
+
+    public float startPos;
+    public float endPos;
+    private float _diff;
+
+    private float _startTime;
+
+    private RectTransform rt;
+
+    private void Awake()
+    {
+        _startTime = Time.time;
+        _diff = endPos - startPos;
+        rt = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        var elapsedTime = Time.time - _startTime;
+        var newX = Mathf.Clamp(startPos + elapsedTime / MaxTime * _diff,
+            startPos,
+            endPos);
+
+        var position = rt.position;
+        var newPos = new Vector3(
+            newX,
+            position.y,
+            position.z);
+
+        rt.SetPositionAndRotation(
+            newPos,
+            Quaternion.identity
+        );
+
+        if (elapsedTime > MaxTime)
+        {
+            if (NetworkController.Instance != null)
+                NetworkController.Instance.EndGame();
+        }
+    }
+}
